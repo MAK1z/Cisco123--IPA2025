7e1d5fe62cabe1429fead8ff843691d022d5cdf4
kridsanapakron <dgsx.64854@gmail.com>
2025-11-02 14:03:21 +0700

    Add handle error to modify interface

--- worker/modify_interface_module/callback.py
+++ worker/modify_interface_module/callback.py
@@ -1,29 +1,35 @@
 from bson import json_util, ObjectId
 from .restconf_client_write import configure_interface
-from database import delete_interface_config
+from database import delete_interface_config, get_db_collection
 
 
 def callback(ch, method, props, body):
     job = json_util.loads(body.decode())
-
+    mycol_interface_status, client = get_db_collection("interface_status")
+    router_ip = job.get("router_ip")
     try:
-        success, msg = configure_interface(
-            router_ip=job.get("router_ip"),
+        configure_interface(
+            router_ip=router_ip,
             username=job.get("username"),
             password=job.get("password"),
             iface_name=job.get("iface_name"),
             iface_ip=job.get("iface_ip"),
             iface_mask=job.get("iface_mask"),
             is_enabled=job.get("is_enabled"),
+            last_config=list(
+                mycol_interface_status.find({"router_ip": router_ip})
+                .sort("timestamp", -1)
+                .limit(1)
+            ),
         )
-        if success:
-            doc_id = job.get("_id")
-            if isinstance(doc_id, dict) and "$oid" in doc_id:
-                doc_id = ObjectId(doc_id["$oid"])
-            elif isinstance(doc_id, str):
-                doc_id = ObjectId(doc_id)
-            elif not isinstance(doc_id, ObjectId):
-                raise ValueError("Invalid _id format")
-            delete_interface_config(doc_id)
+
+        doc_id = job.get("_id")
+        if isinstance(doc_id, dict) and "$oid" in doc_id:
+            doc_id = ObjectId(doc_id["$oid"])
+        elif isinstance(doc_id, str):
+            doc_id = ObjectId(doc_id)
+        elif not isinstance(doc_id, ObjectId):
+            raise ValueError("Invalid _id format")
+        delete_interface_config(doc_id)
     except Exception:
         print("Error")
--- worker/modify_interface_module/restconf_client_write.py
+++ worker/modify_interface_module/restconf_client_write.py
@@ -8,7 +8,14 @@ requests.packages.urllib3.disable_warnings()
 
 
 def configure_interface(
-    router_ip, username, password, iface_name, iface_ip, iface_mask, is_enabled
+    router_ip,
+    username,
+    password,
+    iface_name,
+    iface_ip,
+    iface_mask,
+    is_enabled,
+    last_config,
 ):
     url = f"https://{router_ip}/restconf/data/ietf-interfaces:interfaces/interface={iface_name}"
 
@@ -19,10 +26,8 @@ def configure_interface(
 
     if "Loopback" in iface_name:
         iface_type = "iana-if-type:softwareLoopback"
-    elif "Gigabit" in iface_name:
+    elif "Gigabit" in iface_name or "Ethernet" in iface_name:
         iface_type = "iana-if-type:ethernetCsmacd"
-    elif "Vlan" in iface_name:
-        iface_type = "iana-if-type:l2vlan"
     else:
         iface_type = "iana-if-type:ethernetCsmacd"
 
@@ -48,12 +53,34 @@ def configure_interface(
             if get_resp.status_code == 200:
                 ipv4_data = get_resp.json()
                 addresses = ipv4_data.get("ietf-ip:ipv4", {}).get("address", [])
+                disable_payload = {
+                    "ietf-interfaces:interface": {
+                        "name": iface_name,
+                        "type": iface_type,
+                        "enabled": is_enabled,
+                    }
+                }
+                if not addresses:
+                    requests.put(
+                        url,
+                        headers=headers,
+                        auth=(username, password),
+                        data=json.dumps(disable_payload),
+                        verify=False,
+                    )
                 for addr in addresses:
                     ip = addr.get("ip")
                     if ip:
                         del_url = f"{ipv4_url}/address={ip}"
                         requests.delete(
                             del_url, auth=(username, password), verify=False
+                        )
+                        requests.put(
+                            url,
+                            headers=headers,
+                            auth=(username, password),
+                            data=json.dumps(disable_payload),
+                            verify=False,
                         )
         else:
             ipv4_url = f"https://{router_ip}/restconf/data/ietf-interfaces:interfaces/interface={iface_name}/ietf-ip:ipv4"
@@ -81,8 +108,54 @@ def configure_interface(
         return True, "Configuration successful"
 
     except requests.exceptions.RequestException as e:
-        print("Error configuring")
         error_message = f"Error configuring {iface_name} on {router_ip}: {e}"
         if e.response is not None:
             error_message += f" | Details: {e.response.text}"
+        try:
+            ipv4_url = f"https://{router_ip}/restconf/data/ietf-interfaces:interfaces/interface={iface_name}/ietf-ip:ipv4"
+            current_resp = requests.get(
+                ipv4_url, auth=(username, password), headers=headers, verify=False
+            )
+
+            if current_resp.status_code == 200:
+                data = current_resp.json()
+                addresses = data.get("ietf-ip:ipv4", {}).get("address", [])
+                if addresses:
+                    for addr in addresses:
+                        ip = addr.get("ip")
+                        if ip:
+                            del_url = f"{ipv4_url}/address={ip}"
+                            requests.delete(
+                                del_url, auth=(username, password), verify=False
+                            )
+            if last_config:
+                prev_doc = last_config[0]
+                for intf in prev_doc.get("interfaces", []):
+                    if intf.get("interface") == iface_name:
+                        prev_ip = intf.get("ip_address")
+                        prev_mask = intf.get("netmask")
+                        if prev_ip and prev_mask:
+                            rollback_payload = {
+                                "ietf-interfaces:interface": {
+                                    "name": iface_name,
+                                    "type": iface_type,
+                                    "enabled": True,
+                                    "ietf-ip:ipv4": {
+                                        "address": [
+                                            {"ip": prev_ip, "netmask": prev_mask}
+                                        ]
+                                    },
+                                }
+                            }
+                            requests.put(
+                                url,
+                                headers=headers,
+                                auth=(username, password),
+                                data=json.dumps(rollback_payload),
+                                verify=False,
+                            )
+                            break
+
+        except Exception as recovery_err:
+            print(f"Rollback failed: {recovery_err}")
         return False, error_message
